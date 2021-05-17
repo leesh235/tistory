9f259ff9b3cfeb85c90693f1a481bb7205d4d862
leesh235 <leeshgh235@gmail.com>
2021-05-17 23:53:56 +0900

    base

---
+++ index.js
@@ -0,0 +1,5 @@
+import {GraphQLServer} from "graphql-yoga"
+console.log("Graphql Server Running")
+// const server = new GraphQLServer({})
+
+// server.start(() => console.log("Graphql Server Running"));
