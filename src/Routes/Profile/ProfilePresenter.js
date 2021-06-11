b4fa2eb9ff41feb468637d8bed5a11091bc09462
leesh235 <leeshgh235@gmail.com>
2021-06-11 23:16:14 +0900

    Modify profilepresenter

--- src/Routes/Profile/ProfilePresenter.js
+++ src/Routes/Profile/ProfilePresenter.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const ProfileWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+`;
+
+const ProfileContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 70vh;
+    width: 70vh;
+    border-radius: 15px;
+    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+`;
+
+const UserInfo = styled.div`
+    margin-bottom: 15px
+`;
+
 const ProfilePresenter = ({userInfo}) => {
     return (
-        <div>
-            <div>{userInfo.userId}</div>
-            <div>{userInfo.email}</div>
-        </div>
+        <ProfileWrapper>
+            <ProfileContainer>
+                <UserInfo>{`아이디:           ${userInfo.userId}`}</UserInfo>
+                <UserInfo>{`email:            ${userInfo.email}`}</UserInfo>
+            </ProfileContainer>
+        </ProfileWrapper>
     );
 }
 
--- src/apollo/tokenQuery.js
+++ src/apollo/tokenQuery.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const TOKENINFO = gql`
-{
+query{
     isLoggedIn @client
 }
 `;
