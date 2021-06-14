d340219e14a9eb05b70ab23b19ba2f47e7086184
leesh235 <leeshgh235@gmail.com>
2021-06-14 21:43:16 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import multer from "multer";
+import bodyParser from "body-parser";
 import path from "path";
+import fs from "fs-extra";
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 //cors설정
 const corsOptions = {
@@ -21,20 +23,68 @@ app.use(express.static('uploads'));
 const storage = multer.diskStorage({
     destination: "./uploads",
     filename: (req, file, cb) => {
-        cb(null, file.originalname);
+        cb(null, file.originalname+ "." + Date.now());
     }
 });
+
 //파일 저장경로 지정 및 크기제한
-const uploadImg = multer({
+const upload = multer({
     storage: storage,
     limits:{
         filessize: 100 * 1024 * 1024
     }
 });
 
-app.get('/upload', ({req, res}) => {
-    res.render('upload');
-});
+//해당 주소에서 image 받기
+app.post("/profile", upload.single("streamfile"), async(req, res) => {
+    // try{
+    //     if (fs.existsSync("./uploads/userprofile4")) {
+    //         fs.removeSync("./uploads/userprofile4");
+    //     }
+    //     fs.mkdirSync("./uploads/userprofile4");
+    //     fs.writeFile(
+    //         "./uploads/userprofile4" + "/profile.html",
+    //         '<p><img src="' + req.file + '" alt="image"></p>'
+    //     );
+    //     console.log("success!");
+    // }catch(error){
+    //     console.log(error);
+    // }
+    console.log(req.body.streamfile[0])
+})
+
+app.get("/", async(req, res) => {
+    try{
+        if(fs.existsSync("userprofile2")){
+            const postName = fs.readdirSync("userprofile2")[0];
+            // console.log(postName);
+            const postPath = path.join("userprofile2", postName);
+            res.download(postPath, postName, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("download success");
+                }
+            });
+        }
+        
+    }catch(error){
+        console.log(error);
+    }
+})
+
+// app.post("/add", upload.single("streamfile"), async(req, res) => {
+    
+// })
+
+// //해당 주소로 image보내기
+// app.get("/profile", async(req, res) => {
+    
+// })
+
+// app.get("/detail/:postId", async(req, res) => {
+//     const postId = req.params.postId;
+// })
 
 //서버 실행알림
 app.listen(PORT, () => {
