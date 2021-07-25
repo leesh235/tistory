1fddcfc311ded12d147f179739f07ad305fb190d
leesh235 <leeshgh235@gmail.com>
2021-07-25 22:47:43 +0900

    modify query

--- src/modules/query.js
+++ src/modules/query.js
@@ -10,7 +10,7 @@ export const MODIFYUERIMG = gql`
 
 export const MODIFYPOSTIMG = gql`
     mutation ModifyUserImg($postImg: String){
-        ModifyUserImg(postImg: $userImg){
+        ModifyPostImg(postImg: $postImg){
             postImgId
         }
     }
