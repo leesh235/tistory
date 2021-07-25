89124a7488171927df87221050836c3aeebc5264
leesh235 <leeshgh235@gmail.com>
2021-07-25 22:50:05 +0900

    add post image upload and download

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 import path from "path";
 import fs from "fs-extra";
 import morgan from "morgan";
-import { profileToDB } from "./modules/uploadImg"
+import { profileToDB, postToDB } from "./modules/uploadImg"
 import { upload } from "./middleware/multer"
 
 const app = express();
@@ -20,14 +20,14 @@ const corsOptions = {
 //middleware 등록
 app.use(cors(corsOptions));
 app.use(morgan('dev'));
+app.use(express.json());
 
 //정적자원 이용
 app.use(express.static('uploads'));
-app.use(express.static('posts'));
 
 
 //profile image
-app.post("/profile", upload, async(req, res) => {
+app.post("/profile", async(req, res) => {
     try{
         upload(req, res, async(err) => {
             const {data} = await profileToDB(req);
@@ -53,11 +53,11 @@ app.post("/profile", upload, async(req, res) => {
 app.get("/profileImg/:userId", async(req, res) => {
     try{
         const userId = req.params.userId
-        // console.log("userId: ", userId)
         const dirpath = `uploads/${userId}`;
         const filename = fs.readdirSync(dirpath)[0];
         if(filename){
-            const uri = dirpath+ "/" + filename
+            //uploads를 지정해줄 필요없음, 지정하면 오류발생
+            const uri = userId+ "/" + filename
             res.status(200).send({profileImg: uri});
         }else{
             res.status(404).send({message: "no such image"});
@@ -68,6 +68,44 @@ app.get("/profileImg/:userId", async(req, res) => {
 })
 
 //post image
+app.post("/post", async(req, res) => {
+    try{
+        upload(req, res, async(err) => {
+            const {data} = await postToDB(req);
+
+            if(data.ModifyPostImg.postImgId === null){
+                return false;
+            }
+
+            if(req.file){
+                // console.log(req.file)
+                console.log("success!");
+            } else {
+                console.log("no such file");
+            }
+        })
+    }catch(error){
+        console.log("upload fail");
+        console.log(error);
+    }
+})
+
+app.get("/postImg/:postId", async(req, res) => {
+    try{
+        const postId = req.params.postId
+        const dirpath = `uploads/${postId}`;
+        const filename = fs.readdirSync(dirpath)[0];
+        if(filename){
+            //uploads를 지정해줄 필요없음, 지정하면 오류발생
+            const uri = postId+ "/" + filename
+            res.status(200).send({postImg: uri});
+        }else{
+            res.status(404).send({message: "no such image"});
+        }
+    }catch(err){
+        res.status(500).send({message: `${err}`})
+    }
+})
 
 //서버 실행알림
 app.listen(PORT, () => {
