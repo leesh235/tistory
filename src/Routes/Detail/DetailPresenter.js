27b9f06cd9e1fbc9e9a93501cc2b29c3923469f5
leesh235 <leeshgh235@gmail.com>
2021-07-26 19:34:27 +0900

    modify css

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -3,92 +3,110 @@ import styled from 'styled-components'
 import moment from "moment";
 import { Link } from 'react-router-dom';
 
-const Mcontents = styled.div`
+const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     width: auto;
     height: auto;
     align-items: center;
-    margin: 30px 100px 50px 100px;
+    margin: 200px 0px;
 `;
 
-const HeaderSize = styled.div`
-    width:1000px;
-    height: 200px;
-    margin-top:100px;
-    margin-bottom:100px;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    border-radius: 10px;
-`;
-
-const HeaderStyle = styled.div`
-    width: 100%;
-    max-width: 700px;
-    display: flex;
-    flex-direction: row;
-`;
-
-const Mdiv = styled.div`
+const Top = styled.div`
     display: flex;
     flex-direction: column;
-    margin: 30px 0px 10px 50px;
+    justify-content: center;
+    width: 900px;
+`;
+
+const LineStyle = styled.div`
+    width: 100%;
+    border-bottom: 1px solid gray;
+    margin: 12px 0px;
+`;
+
+const TitleStyle = styled.h2`
+
+`;
+
+const ContentInfo = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    >:nth-child(n){
+        margin-left: 12px;
+    }
 `;
 
 const DateStyle = styled.div`
-    color: red;
-    justify-content:center
+
 `;
 
-const TitleStyle = styled.h1`
-    margin-left: 50px;
-    margin-bottom: 10px;
+const UserNameStyle = styled.div`
+
 `;
 
-
-const UserNameStyle = styled.h4`
-    margin-left: 60px;
-    margin-bottom: 60px;
-    color: red;
+const ContentWrapper = styled.div`
+    width: 800px;
+    min-height: 100px;
+    margin: 50px 0px;
 `;
 
-const ContentsStyle = styled.div`
-    width: 1200px;
-    height: auto;
-    font-size:22px;
+const BtnWrapper = styled.div`
+    width: 800px;
+    display: flex;
+    flex-direction: row-reverse;
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    >:nth-child(n){
+        margin-left: 30px;
+    }
 `;
 
-const PostBtn = styled.button`
+const PostBtn = styled.div`
 
 `;
 
 const DetailPresenter = ({postImg, post, postId, equal}) => {
 
     return(
-        <Mcontents>
-            <HeaderSize>
-                <HeaderStyle>
-                    <Mdiv>
-                        <TitleStyle>{post.title}</TitleStyle>
-                        <UserNameStyle>{post.id}</UserNameStyle>
-                    </Mdiv>
-                    <DateStyle>{moment(post.createdAt).format("YYYY-MM-DD / LT")}</DateStyle>
-                </HeaderStyle>
-            </HeaderSize>
-            <ContentsStyle>{post.contents}</ContentsStyle>
-            {postImg ? <img src={`http://localhost:5000/${postImg}`} /> : ""}
-            {equal ? 
-                <Link to={{
-                    pathname: `/modifyPost/${postId}`,
-                    state:{
-                        title: post.title,
-                        contents: post.contents
-                    }
-                }}>
-                    <PostBtn>수정</PostBtn>
-                </Link> :
-                ""
-            }
-        </Mcontents>
+        <Wrapper>
+            <Top>
+  
+                <TitleStyle>{post.title}</TitleStyle>
+
+                <LineStyle></LineStyle>
+
+                <ContentInfo>
+                    <DateStyle>{`작성일: ${moment(post.createdAt).format("YYYY-MM-DD LT")}`}</DateStyle>
+                    <UserNameStyle>{`작성자: ${post.id}`}</UserNameStyle>
+                </ContentInfo>
+
+
+            </Top>
+
+            <ContentWrapper>
+                {post.contents}
+                {postImg ? <img src={`http://localhost:5000/${postImg}`} /> : ""}
+            </ContentWrapper>
+
+            <BtnWrapper>
+                {equal ? <PostBtn>삭제</PostBtn> : ""}
+                {equal ? 
+                    <Link to={{
+                        pathname: `/modifyPost/${postId}`,
+                        state:{
+                            title: post.title,
+                            contents: post.contents
+                        }
+                    }}>
+                        <PostBtn>수정</PostBtn>
+                    </Link> :
+                    ""
+                }
+            </BtnWrapper>
+        </Wrapper>
     );
 }
 
