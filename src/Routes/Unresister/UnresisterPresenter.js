4078e2e4b9704905ac1a0c9c5a6ee85d065d662e
leesh235 <leeshgh235@gmail.com>
2021-08-03 00:01:24 +0900

    modify css

--- src/Routes/Unresister/UnresisterPresenter.js
+++ src/Routes/Unresister/UnresisterPresenter.js
@@ -1,8 +1,64 @@
 import React from 'react';
+import styled from "styled-components";
 
-const UnresisterPresenter = () => {
+const Input = styled.input`
+    height: 40px;
+    background-color: #ffffff;
+    border: solid 1px #dadada;
+    margin-bottom: 15px;
+    font-size: 15px;
+    padding: 10px;
+`;
+
+const Btn = styled.div`
+    width: 460px;
+    height: 60px;
+    background-color: #white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #8e8e8e;
+    margin-top: 15px;
+    margin-bottom: 15px;
+    font-size: 20px;
+    font-weight: 700;
+    cursor: pointer;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+`;
+const FormWrapper = styled.div`
+    display: flex;
+    margin-bottom: 10px;
+    flex-direction: column;
+`;
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 80vh;
+    flex-direction: column;
+`;
+
+const UnresisterPresenter = ({ password, passwordConfirm, onClick }) => {
         return(
-                <div>회원탈퇴</div>
+            <form>
+                <Wrapper>
+                    <Container>
+                        <FormWrapper>
+                            <Input placeholder={"  비밀번호"} {...password} type={"password"}></Input>
+                            <Input placeholder={"  비밀번호 확인"} {...passwordConfirm} type={"password"}></Input>
+                            <Btn onClick={onClick}>회원탈퇴 하기</Btn>                   
+                        </FormWrapper> 
+                    </Container>
+                </Wrapper>
+            </form>
         );
 }
 
