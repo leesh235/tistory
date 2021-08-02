1425ea5f89f0b07c95484b22566f2b9d851b0845
leesh235 <leeshgh235@gmail.com>
2021-08-03 00:01:45 +0900

    add log out mutation

--- src/Routes/Unresister/UnresisterContainer.js
+++ src/Routes/Unresister/UnresisterContainer.js
@@ -2,29 +2,44 @@ import React from 'react';
 import UnresisterPresenter from './UnresisterPresenter';
 import { useMutation } from '@apollo/client';
 import { UNRESISTER } from "./UnresisterQuery";
-import { useInput } from "../../Hooks/useInput";
+import useInput from "../../Hooks/useInput";
+import { TOKENLOGOUT } from "../../apollo/tokenQuery";
 
 const UnresisterContainer = ({props}) => {
+
     const { state } = props.history.location
-    console.log(state)
+    // console.log(state)
     const [setUnresister] = useMutation(UNRESISTER);
+    const [tokenMutation] = useMutation(TOKENLOGOUT);
 
     const password = useInput("");
+    const passwordConfirm = useInput("");
 
     const onClick = async() => {
-        if(window.confirm("회원탈퇴를 하시겠습니다?")){
-            await setUnresister({
-                variables:{
-                    userId: state.userInfo.userId,
-                    password
+        if(password.value !== passwordConfirm.value){
+            window.alert("비밀번호가 다릅니다.")
+        }else{
+            if(window.confirm("회원탈퇴를 하시겠습니다?")){
+                const { data : {Unresister} } = await setUnresister({
+                    variables:{
+                        userId: state.userInfo.userId,
+                        password: password.value
+                    }
+                })
+                if(Unresister){
+                    await tokenMutation();
+                    window.location.replace("/");
+                }else{
+                    window.alert("비밀번호가 틀렸습니다.")
                 }
-            })
+            }
         }
     }
 
     return(
             <UnresisterPresenter 
                 password={password}
+                passwordConfirm={passwordConfirm}
                 onClick={onClick}
             />
     );
