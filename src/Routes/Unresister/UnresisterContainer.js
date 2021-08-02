4ced7d1db49c744f0e913cc900b744594db5abc3
leesh235 <leeshgh235@gmail.com>
2021-08-02 23:45:49 +0900

    connect server

--- src/Routes/Unresister/UnresisterContainer.js
+++ src/Routes/Unresister/UnresisterContainer.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import UnresisterPresenter from './UnresisterPresenter';
+import { useMutation } from '@apollo/client';
+import { UNRESISTER } from "./UnresisterQuery";
+import { useInput } from "../../Hooks/useInput";
 
-const UnresisterContainer = (props) => {
-        return(
-                <UnresisterPresenter />
-        );
+const UnresisterContainer = ({props}) => {
+    const { state } = props.history.location
+    console.log(state)
+    const [setUnresister] = useMutation(UNRESISTER);
+
+    const password = useInput("");
+
+    const onClick = async() => {
+        if(window.confirm("회원탈퇴를 하시겠습니다?")){
+            await setUnresister({
+                variables:{
+                    userId: state.userInfo.userId,
+                    password
+                }
+            })
+        }
+    }
+
+    return(
+            <UnresisterPresenter 
+                password={password}
+                onClick={onClick}
+            />
+    );
 }
 
 export default UnresisterContainer;
