db02815e7e215b8147159dfee833af40029ecb16
leesh235 <leeshgh235@gmail.com>
2021-08-18 19:21:20 +0900

    create ckeditor component

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,8 +1,7 @@
-import React,{ useState } from "react";
-import styled from 'styled-components';
-
+import React, { useEffect, useImperativeHandle, useState } from 'react';
 import { CKEditor} from '@ckeditor/ckeditor5-react';
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
@@ -18,85 +17,95 @@ import List from "@ckeditor/ckeditor5-list/src/list";
 import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
 import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import styled from 'styled-components';
 
-const Editor = (props) => {
-    ClassicEditorBase.builtinPlugins = [
-        Essentials,
-        Bold,
-        Italic,
-        BlockQuote,
-        Heading,
-        Image,
-        ImageStyle,
-        ImageToolbar,
-        ImageUpload,
-        Link,
-        List,
-        Paragraph,
-        Alignment,
-        SimpleUploadAdapter,
-        MediaEmbed             
-      ];
-      
-      ClassicEditorBase.defaultConfig = {
-        toolbar: {
-          items: [
-            'heading',
-            '|',
-            'alignment', 
-            '|',
-            'mediaEmbed',
-            '|',
-            'imageUpload',
-            'undo',
-            'redo',
-          ]
-        },
-        mediaEmbed: {
-          previewsInData: true
-        },
-        heading : {
-          options:[
-            { model: 'paragraph', view: 'p', title: '본문', class: 'ck-heading_paragraph' },
-            { model: 'heading1', view: 'h1', title: '타이틀', class: 'ck-heading_heading1' },
-            { model: 'heading2', view: 'h2', title: '서브 타이틀', class: 'ck-heading_heading2' }
-          ]
-        },
-        image: {
-          // Configure the available styles.
-          styles: [
-            'alignLeft', 'alignCenter', 'alignRight'
-          ],
-          // Configure the available image resize options.
-          toolbar: [
-            'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
-            '|',
-            'imageTextAlternative'
-          ]
-        },
-        simpleUpload: {
-        //   uploadUrl: `${uploader.uploadServer}/upload/editorimg`,
-          headers: {
-            // 'email': `${email}`,
-            // 'projectid' : `${projectId}`,
-            // 'type' : `${type}`
-          }
-        },
-        language: 'en'
-      };
-      const [editorData, setEditorData] = useState("");
+const Wrapper = styled.div`
+
+`
+
+export const Editor = ({getFormData}) => {
+  
+  ClassicEditorBase.builtinPlugins = [
+    Essentials,
+    Bold,
+    Italic,
+    BlockQuote,
+    Heading,
+    Image,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Link,
+    List,
+    Paragraph,
+    Alignment,
+    SimpleUploadAdapter,
+    MediaEmbed             
+  ];
+  
+  ClassicEditorBase.defaultConfig = {
+    toolbar: {
+      items: [
+        'heading',
+        '|',
+        'alignment', 
+        '|',
+        'mediaEmbed',
+        '|',
+        'imageUpload',
+        'undo',
+        'redo',
+      ]
+    },
+    mediaEmbed: {
+      previewsInData: true
+    },
+    heading : {
+      options:[
+        { model: 'paragraph', view: 'p', title: '본문', class: 'ck-heading_paragraph' },
+        { model: 'heading1', view: 'h1', title: '타이틀', class: 'ck-heading_heading1' },
+        { model: 'heading2', view: 'h2', title: '서브 타이틀', class: 'ck-heading_heading2' }
+      ]
+    },
+    image: {
+      // Configure the available styles.
+      styles: [
+        'alignLeft', 'alignCenter', 'alignRight'
+      ],
+      // Configure the available image resize options.
+      toolbar: [
+        'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
+        '|',
+        'imageTextAlternative'
+      ]
+    },
+    simpleUpload: {
+      uploadUrl: `http://localhost:5000/editorImg`,
+      // headers: {
+      //   user: "hi"
+      // }
+    },
+    language: 'en'
+  };
+
+
+  const [editorData, setEditorData] = useState("");
+  
+  
+  useEffect(()=> {
+
+  },[])
+
   return (
-    <div>
-             <CKEditor 
-        editor={ClassicEditorBase}
-        data={editorData}
-        onChange={(event, editor) => {
-          console.log(editor.getData());
-        //   setData(editor.getData());
-        }}
-        /> 
-    </div>
+    <Wrapper>
+      <CKEditor 
+      editor={ClassicEditorBase}
+      data={editorData}
+      onChange={(event, editor) => {
+        console.log(editor.getData());
+        getFormData(editor.getData())
+      }}
+      /> 
+    </Wrapper>
   );
-};
-
-export default Editor;
+}
