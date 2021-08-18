0eeedadcede6ac9a2b55c435afd656b399b5e306
leesh235 <leeshgh235@gmail.com>
2021-08-18 20:41:03 +0900

    fix bug

--- src/Routes/Add/AddContainer.js
+++ src/Routes/Add/AddContainer.js
@@ -3,60 +3,46 @@ import { useMutation } from "@apollo/client";
 import { ADD } from "./AddQuery";
 import AddPresenter from './AddPresenter';
 import useInput from '../../Hooks/useInput';
-import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 const AddContainer = (props) => {
 
     const titleInput = useInput("");
-    const contentsInput = useInput("");
-    const [picture, setPicture] = useState("");
+
     const [postData, setPostData] = useState("");
 
     const [AddMutation] = useMutation(ADD);
-
-    const history = useHistory();
-
-    const handlePicture = (e) => {
-        e.preventDefault();
-        const image = e.target.files[0];
-        // console.log(image);
-        // console.log(props)
-        setPicture(image);
-      }
 
     const onSubmit = async(e) => {
         e.preventDefault();
 
         try{
             if(titleInput.value !== ""){
-                const { data: { createPost : {postId} } } = await AddMutation({
+                const { data: { createPost : {postId, status} } } = await AddMutation({
                     variables: {
                         title: titleInput.value,
-                        contents: contentsInput.value
+                        contents: postData
                     }
                 });
-                console.log("postId: ", postId)
-                if(postId){
-                    alert("작성 완료");
-                    window.location.replace("/");
-                    if(picture !== undefined && picture !== null){
-                        const jwt = localStorage.getItem("token");
-                        const formData = new FormData();
-                        formData.append("user", postId);
-                        formData.append("streamfile", picture);
-                        formData.append("editor", postData);
-                    
-                        await axios({
-                          method: "post",
-                          url: "http://localhost:5000/editor",
-                          data: formData,
-                          headers: {
-                            Authorization: jwt,
-                            "Content-Type": "multipart/form-data",
-                          }
-                        })
-                    }
+                // console.log("postId: ", postId, status)
+                // alert("작성 완료");
+                // window.location.replace("/");
+                if(postId !== null){
+                    const jwt = localStorage.getItem("token");
+                    const formData = new FormData();
+                    formData.append("postId", postId);
+                    formData.append("title", titleInput.value);
+                    formData.append("editor", postData);
+                
+                    await axios({
+                        method: "post",
+                        url: "http://localhost:5000/editor",
+                        data: formData,
+                        headers: {
+                        Authorization: jwt,
+                        "Content-Type": "multipart/form-data",
+                        }
+                    })
                 }
             }else {
                 alert("제목을 작성해주세요.");
@@ -70,11 +56,10 @@ const AddContainer = (props) => {
         
         <AddPresenter 
             title={titleInput}
-            contents={contentsInput}
-            handlePicture={handlePicture}
             onSubmit={onSubmit}
             setPostData={setPostData}
         />
+ 
     );
 }
 
