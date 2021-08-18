fcfa71228ac59dc17cbbb2d4ad2d2c92035d73ab
leesh235 <leeshgh235@gmail.com>
2021-08-18 20:28:32 +0900

    add editor apies

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import bodyParser from "body-parser";
 import path from "path";
 import fs from "fs-extra";
 import morgan from "morgan";
-import { profileToDB, postToDB } from "./modules/uploadImg"
-import { upload } from "./middleware/multer"
+import { profileToDB, postImageToDB, postContentsToDB } from "./modules/uploadImg"
+import { upload, imageUpload } from "./middleware/multer"
 
 const app = express();
 const PORT = 5000;
@@ -68,40 +68,72 @@ app.get("/profileImg/:userId", async(req, res) => {
 })
 
 //editor
+//editor를 통해 들어온 image저장
+app.post("/editorImg", async(req, res) => {
+    try{
+        imageUpload(req, res, async(err) => {
+            console.log("body: ",req.body)
+            console.log("headers: ",req.headers)
+            console.log("file: ",req.file)
+            // const { data } = await postImageToDB(req)
+
+        })
+    }catch(err){
+        console.log(err)
+    }
+})
+
+//editor를 통해 들어온 text 저장
 app.post("/editor", async(req, res) => {
-    console.log("editor: ",req.body)
     try{
         upload(req, res, async(err) => {
-            const {data} = await postToDB(req);
+            console.log("body: ",req.body)
+            // console.log("file: ",req.file)
+            const {data : { uploadText : { check, status } }} = await postContentsToDB(req);
+            if(check){
+                const dirPath = `./uploads/${req.body.postId}`;
+                const fileData = req.body.editor
+                let fileContents = Buffer.from(fileData, "utf8");
 
-            if(data.ModifyPostImg.postImgId === null){
+                if (fs.existsSync(dirPath)) {
+                    fs.removeSync(dirPath);
+                }
+
+                fs.mkdirSync(dirPath);
+                fs.writeFile(dirPath+ "/" + req.body.title + ".html", fileContents)
+            }else{
                 console.log("실패")
                 return false;
             }
-
-            console.log("editor: ",req.body)
-            const fileData = req.body.editor
-            var fileContents = Buffer.from(fileData, "utf8");
-            console.log("fileContents: ", fileContents)
-            const dirpath = `uploads/${req.body.user}`;
-            fs.mkdirSync(dirpath);
-            fs.writeFile(dirpath+ "/" + req.body.user + ".html", fileContents)
-            // `
-            // <!DOCTYPE html>
-            // <html>
-            //     <head></head>
-            //     <body>
-            //         ${req.body.editor}
-            //     </body>
-            // </html>
-            // `)
-            console.log(req.body.editor);
-            console.log(req.body.user);
         })
     }catch(error){
         console.log(error);
     }
 })
+
+// app.post("/editor", async(req, res) => {
+//     try{
+//         upload(req, res, async(err) => {
+//             console.log("body: ",req.body)
+//             console.log("headers: ",req.headers)
+//             const {data} = await postToDB(req);
+//             if(data.ModifyPostImg.postImgId === null){
+//                 console.log("실패")
+//                 return false;
+//             }
+//             postId = req.body.user;
+//             const fileData = req.body.editor
+//             var fileContents = Buffer.from(fileData, "utf8");
+//             // console.log("fileContents: ", fileContents)
+//             console.log("postId: ", tempoFile)
+//             const dirpath = `uploads/${req.body.user}`;
+//             fs.mkdirSync(dirpath);
+//             fs.writeFile(dirpath+ "/" + req.body.user + ".html", fileContents)
+//         })
+//     }catch(error){
+//         console.log(error);
+//     }
+// })
 
 app.get("/editor/:postId", async(req, res) => {
     try{
