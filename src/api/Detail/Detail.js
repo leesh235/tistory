126adbf3bc0a51287e002946fa248f454b07452c
leesh235 <leeshgh235@gmail.com>
2021-08-18 21:33:07 +0900

    modify query

--- src/api/Detail/Detail.js
+++ src/api/Detail/Detail.js
@@ -5,49 +5,58 @@ const prisma = new PrismaClient();
 
 export default {
     Query: {
-        getPost: async(_, args, {request}) => {
+        getPostDetail: async(_, args, {request}) => {
             try{
                 const exist = isAuthenticated(request);
-                // console.log(exist)
-                // console.log(request.user.userId)
+
                 if(exist === true){
-                    const { postId, id } = args;
-                    // console.log(postId)
+                    const { postId, email } = args;
+
                     const post = await prisma.post.findUnique({
-                        where: { postId }
+                        where: { 
+                            postId: Number(postId)
+                        }
                     })
                     
                     if(post !== null){
                         // console.log(post);
-                        if(request.user.userId === id){
+                        if(request.user.email === email){
                             return {
-                                equal: true,
-                                Post: post
-                            };
+                                    equal: true,
+                                    status: "success and writer",
+                                    Post: post
+                                };
                         }else{
                             return {
                                 equal: false,
+                                status: "success",
                                 Post: post
                             };
                         }
                     }else{
                         return {
                             equal: false,
+                            status: "no post",
                             Post: null
-                        }
+                        };
                     }
 
                 }else{
                     console.log("You need to log in to perform this action");
                     return {
                         equal: false,
-                        getPost: null
-                    }
+                        status: "not log in",
+                        Post: null
+                    };
                 }
 
             }catch(error){
-                console.log(error);
-                return null;
+                console.log(error.reponse);
+                return {
+                    equal: false,
+                    status: "server error",
+                    Post: null
+                };
             }
         }
     }
