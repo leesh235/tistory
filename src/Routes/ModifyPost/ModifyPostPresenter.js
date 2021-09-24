082b3b582d5c8dfdb64f7fe2a68cf9c339e41a32
leesh235 <leeshgh235@gmail.com>
2021-09-24 22:53:36 +0900

    modify css

--- src/Routes/ModifyPost/ModifyPostPresenter.js
+++ src/Routes/ModifyPost/ModifyPostPresenter.js
@@ -1,37 +1,20 @@
 import React from "react";
 import styled from "styled-components";
-// import {Editor} from "../../components/Editor";
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
 
-const BodyStyle = styled.div`
-    width: auto;
-    height: 400px;
-    margin: 30px 100px 50px 100px;
-`;
-
-const BoxStyle = styled.div`
-    height: 400px;
-    width: 500px;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    border-radius: 10px;
-`;
-
-const ContentStyle = styled.div`
+const Wrapper = styled.section`
     display: flex;
     flex-direction: column;
+    align-items: center;
+    width: 100%;
+    margin: 150px 0 0 0;
 `;
 
-const TitleIn = styled.input`
-    height: 30px;
+const Box = styled.div`
     width: 80%;
-    margin-bottom: 20px;
-`;
-
-const SummaryIn = styled.textarea`
-    height: 30px;
-    width: 80%;
-    margin-bottom: 20px;
 `;
 
 const SubmitIn = styled.div`
@@ -41,26 +24,17 @@ const SubmitIn = styled.div`
 
 export default ({ title, onSubmit, handlePicture, setPostData, editorRef }) => {
     return (
-        // <BodyStyle>
-        //     <BoxStyle>
-        //         <ContentStyle>
-        //                 <TitleIn placeholder={"  제목"} {...title} />
-        //                 <Editor getFormData={setPostData} user={"test"} />
-        //                 <SubmitIn onClick={onSubmit}>완료</SubmitIn>
-        //         </ContentStyle>
-        //     </BoxStyle>
-        // </BodyStyle>
-        <>
-        {/* <form onSubmit={onSubmit}> */}
-            <TitleIn placeholder={"  제목"} {...title} />
-            <Editor
-                previewStyle="vertical"
-                height="600px"
-                initialEditType="wysiwyg"
-                ref={editorRef}
-            />
+        <Wrapper>
+            <Input w={"80%"} h={"30px"} margin={"0 0 20px 0"} placeholder={"  제목"} func={title}/>
+            <Box>
+                <Editor
+                    previewStyle="vertical"
+                    height="600px"
+                    initialEditType="wysiwyg"
+                    ref={editorRef}
+                />
+            </Box>
             <SubmitIn onClick={onSubmit}>완료</SubmitIn>
-        {/* </form> */}
-        </>
+        </Wrapper>
     );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,54 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const Wrapper = styled.input`
+    width: ${prop => prop.w};
+    height: ${prop => prop.h};
+    margin: ${prop => prop.margin};
+    padding: ${prop => prop.padding};
+    border: solid 1px ${prop => prop.color};
+    border-radius: 6px;
+
+    color: ${prop => prop.color};
+
+    ::placeholder{
+        color: ${prop => prop.color};
+    }
+
     ${prop => prop.props}
 `;
 
-export const Input = ({props}) => {
+export const Input = ({props, w, h, margin, padding, color, fcolor, placeholer, register, func}) => {
     return(
-        <Wrapper props={props}/>
+        <Wrapper 
+            props={props} 
+            w={w} 
+            h={h} 
+            margin={margin} 
+            padding={padding} 
+            placeholder={placeholer}
+            color={color}
+            fcolor={fcolor}
+            {...register}
+            {...func}
+        />
     );
+}
+
+Input.defaultProps = {
+    color: "gray",
+    fcolor: "black",
+    placeholer: "내용을 입력하세요",
+}
+
+Input.LineStyle = {
+    color: PropTypes.string,
+    fcolor: PropTypes.string,
+    w: PropTypes.string,
+    h: PropTypes.string,
+    margin: PropTypes.string,
+    padding: PropTypes.string,
+    placeholer: PropTypes.string,
+    placeholer: PropTypes.object,
+    placeholer: PropTypes.object,
 }
