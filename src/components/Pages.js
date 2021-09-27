7f23df9836632e77e1c7dd925bbcde07b93e7153
leesh235 <leeshgh235@gmail.com>
2021-09-27 23:56:05 +0900

    add page controller component

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -1,74 +1,44 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, {useState, useEffect} from 'react';
 import styled from "styled-components";
 
-const BodyStyle = styled.div`
-    width: auto;
-    height: auto;
-    margin: 0px 100px 0px 100px
-`;
-
-const PagesContainer = styled.div`
-    margin-top: 100px;
-`;
-
-const PagesStyle = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-`;
-
-const PageStyle = styled.div`
-    margin: 0px 10px 0px 10px;
-`;
-
-const BntStyle = styled.button`
+const Wrapper = styled.div`
 
 `;
 
-export const Pages = ({ getDate, pageNum }) => {
+const Page = styled.div`
 
-    const arr=[1, 2, 3, 4, 5];
+`;
+const UpButton = styled.div`
 
-    const handleMinus = () => {
-        if(arr[0] !== 1){
-            this.setState({
-                arr: arr.map(i => {return i = i - 1;})
-            })
+`;
+const DownButton = styled.div`
+
+`;
+export const Pages = ({ total = 0, each = 0, max = 1 }) => {
+
+    const calculate = total / each + 1;
+
+    const [page, setPage] = useState(max);
+
+    const pageHandler = () => {
+        let arr = [];
+        for(let i = 1; i <= calculate; i++){
+            arr.push(i);
         }
-    }
-
-    const handleAdd = () => {
-        if(pageNum > 5 && arr[4] < pageNum){
-            this.setState({
-                arr: arr.map(i => {return i = i + 1;})
-            })
-        }
+        return arr;
     }
 
     return(
-        <BodyStyle>
-            <PagesContainer>
-                <PagesStyle>
-                    <BntStyle onClick={handleMinus}>◀</BntStyle>
-                    {arr.map(i => {
-                        if(i <= pageNum){
-                            return(
-                                <PageStyle key={i}><Link onClick={() => {
-                                    getDate(i)
-                                }} to={{
-                                    pathname:`/page=${i}`,
-                                    state:{
-                                        i
-                                    }
-                                }}>{i}</Link></PageStyle>
-                            );
-                        }
-                        return "";
-                    })}
-                    <BntStyle onClick={handleAdd}>▶</BntStyle>
-                </PagesStyle>
-            </PagesContainer>
-        </BodyStyle>
+        <Wrapper>
+            <UpButton>up</UpButton>
+            {pageHandler().map((idx) => {
+                if(idx <= max){
+                    return(
+                        <Page key={idx}>{idx}</Page>
+                    );
+                }
+            })}
+            <DownButton>down</DownButton>
+        </Wrapper>
     );
 }
