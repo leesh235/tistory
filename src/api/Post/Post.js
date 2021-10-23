e1a0fa25c90e40787e3247e7f85a6a709a5c6fcb
leesh235 <leeshgh235@gmail.com>
2021-10-23 23:58:49 +0900

    modify page controller

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -4,9 +4,13 @@ const prisma = new PrismaClient();
 
 export default {
     Query: {
-        getAllPosts: async(_,__) => {
+        getAllPosts: async(_,args) => {
             try {
+                const { count, page } = args;
+                console.log(count, page)
                 const post = await prisma.post.findMany({
+                    skip: (page - 1) * count,
+                    take: count,
                     orderBy: {
                         createdAt:"desc"
                     }
