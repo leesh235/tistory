8f67e8e5b164b87f0e1d22268002c512b4d7814d
leesh235 <leeshgh235@gmail.com>
2021-10-25 00:13:00 +0900

    modify query

--- src/api/Search/Search.js
+++ src/api/Search/Search.js
@@ -11,26 +11,42 @@ export default {
 
                 if(text){
                     const getPost = await prisma.post.findMany({})
-    
+                    const getPostLen = getPost.length;
                     //title에 text를 포함하고 있는 post 구하기
                     const getTitle = getPost.filter((post) => {
                         return post.title.includes(text);
                     })
                     // console.log(getTitle);
 
-                    if(getTitle){
-                        return getTitle;
+                    if(getTitle !== null){
+                        return {
+                            search: getTitle,
+                            postCnt: getPostLen,
+                            status: "success"
+                        };
                     }else{
-                        return null;
+                        return {
+                            search: null,
+                            postCnt: 0,
+                            status: "no match"
+                        };
                     }
                     
                 }else{
-                    return null;
+                    return {
+                        search: null,
+                        postCnt: 0,
+                        status: "no search word"
+                    };
                 }
 
             }catch (error){
                 console.log(error);
-                return null;
+                    return {
+                        search: null,
+                        postCnt: 0,
+                        status: "error"
+                    };
             }
         }
     }
