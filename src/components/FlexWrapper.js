b5dad206f37c568d5415ca4d7ad31a5171481c87
leesh235 <leeshgh235@gmail.com>
2021-10-31 19:26:28 +0900

    add props

--- src/components/FlexWrapper.js
+++ src/components/FlexWrapper.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const Wrapper = styled.div`
     display: ${p=>p.display};
@@ -9,8 +10,37 @@ const Wrapper = styled.div`
     height: ${p=>p.h};
 `;
 
-export const FlexWrapper = ({children, props}) => {
+export const FlexWrapper = ({children, props, display, fd, ai, jc, w, h, m}) => {
     return(
-        <Wrapper {...props}>{children}</Wrapper>
+        <Wrapper 
+            props={props}
+            display={display}
+            fd={fd}
+            ai={ai}
+            jc={jc}
+            w={w}
+            h={h}
+            m={m}
+        >{children}</Wrapper>
     );
+}
+
+FlexWrapper.defaultProps = {
+    display: "flex",
+    ai: "flex-start",
+    jc: "flex-start",
+    w: "100%",
+    h: "auto",
+    m: "0px"
+}
+
+FlexWrapper.propTypes = {
+    props: PropTypes.string,
+    display: PropTypes.string,
+    fd: PropTypes.string,
+    ai: PropTypes.string,
+    jc: PropTypes.string,
+    w: PropTypes.string,
+    h: PropTypes.string,
+    m: PropTypes.string,
 }
