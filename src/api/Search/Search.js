e604274adc80436ebc4bd7aa8310cfd455e13071
leesh235 <leeshgh235@gmail.com>
2021-11-01 20:06:23 +0900

    add page args

--- src/api/Search/Search.js
+++ src/api/Search/Search.js
@@ -6,22 +6,34 @@ export default {
     Query: {
         getSearch: async(_, args) => {
             try {
-                const { text } = args;
+                const { count, page, text } = args;
                 //모든 post 불러오기
 
-                if(text){
-                    const getPost = await prisma.post.findMany({})
-                    const getPostLen = getPost.length;
+                if(text !== ""){
+                    const totalPost = await prisma.post.findMany({
+                        where: {
+                            title: {
+                                contains: text
+                            }
+                        }
+                    })
+                    const postLen = totalPost.length;
                     //title에 text를 포함하고 있는 post 구하기
-                    const getTitle = getPost.filter((post) => {
-                        return post.title.includes(text);
+                    const getTitle = await prisma.post.findMany({
+                        skip: (page - 1) * count,
+                        take: count,
+                        where: {
+                            title: {
+                                contains: text
+                            }
+                        }
                     })
                     // console.log(getTitle);
 
                     if(getTitle !== null){
                         return {
                             search: getTitle,
-                            postCnt: getPostLen,
+                            postCnt: postLen,
                             status: "success"
                         };
                     }else{
