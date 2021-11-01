a8106f3a5f3aed2175b62148fe4fba370a4cecba
leesh235 <leeshgh235@gmail.com>
2021-11-01 19:48:12 +0900

    modify query

--- src/api/getMyPost/getMyPost.js
+++ src/api/getMyPost/getMyPost.js
@@ -5,25 +5,40 @@ const prisma = new PrismaClient();
 
 export default {
     Query: {
-        getMyPosts: async(_,__,{request}) => {
+        getMyPosts: async(_,args,{request}) => {
             try {
                 const exist = isAuthenticated(request);
 
                 if(exist){
+                    const { count, page } = args;
+
                     const email = request.user.email
-                    const myPosts = await prisma.post.findMany({
+                    const totalPost = await prisma.post.findMany({
                         where: { writer: email }
+                    })
+                    const postLen = totalPost.length;
+                    const myPosts = await prisma.post.findMany({
+                        skip: (page - 1) * count,
+                        take: count,
+                        where: { 
+                            writer: email 
+                        },
+                        orderBy: {
+                            createdAt:"desc"
+                        }
                     })
                     console.log(myPosts);
 
                     if(myPosts !== null){
                         return {
                             status: "success",
+                            postCnt: postLen,
                             allMyPosts: myPosts
                         }
                     }else{
                         return {
                             status: "no exist my posts",
+                            postCnt: 0,
                             allMyPosts: null
                         }
                     }
@@ -31,6 +46,7 @@ export default {
                     console.log("You need to log in to perform this action");
                     return {
                         status: "not log in",
+                        postCnt: 0,
                         allMyPosts: null
                     }
                 }
@@ -39,6 +55,7 @@ export default {
                 console.log(error);
                 return {
                     status: "server error",
+                    postCnt: 0,
                     allMyPosts: null
                 }
             }
