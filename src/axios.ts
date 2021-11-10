4d1d18d53ec84ec8303f5c20d739651c63552f57
leesh235 <leeshgh235@gmail.com>
2021-11-10 17:30:58 +0900

    modify url

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "./utiles";
 
-const baseURL = process.env.REACT_APP_FILESERVER;
+const baseURL = `${process.env.REACT_APP_FILESERVER}/`;
 
 export const fileserver = axios.create({
     baseURL,
