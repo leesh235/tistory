a0e8a1106045b697fa44d4af0009989a1c7635e2
leesh235 <leeshgh235@gmail.com>
2021-11-10 21:42:58 +0900

    add link

--- src/apollo/apollo.ts
+++ src/apollo/apollo.ts
@@ -1,5 +1,6 @@
 // import ApolloClient from "apollo-boost";
 import { ApolloClient, InMemoryCache, createHttpLink }from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { resolvers } from "./LocalState";
 
 //LocalState.ts의 11번줄, defaults와 비슷
@@ -20,19 +21,24 @@ const cache: InMemoryCache = new InMemoryCache({
 const uri = process.env.REACT_APP_SERVER;
 
 const httpLink = createHttpLink({
-  uri
-})
-
-const client =  new ApolloClient({
   uri,
   credentials: "same-origin",
-  // link: httpLink,
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client =  new ApolloClient({
+  link: authLink.concat(httpLink),
   cache,
   resolvers: resolvers,
-  headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-  },
-  
 });
 
 export default client;
