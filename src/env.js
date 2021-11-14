a7788ee38ea4364527ed3a78ebd97dc3d752e4d0
leesh235 <leeshgh235@gmail.com>
2021-11-14 20:32:22 +0900

    add env file (mode: dev and prod)

--- src/env.js
+++ src/env.js
@@ -1,4 +1,11 @@
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config(path.join(__dirname, "../.env"));
+// dotenv.config(path.join(__dirname, "../.env"));
+
+dotenv.config({
+    path: path.resolve(
+        process.cwd(),
+        process.env.NODE_ENV === "dev" ? '.env.dev' : '.env.prod'
+    )
+});
