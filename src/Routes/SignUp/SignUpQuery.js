6045dc719dae52d0b3d87a4bf097e2262fc4d23a
leesh235 <leeshgh235@gmail.com>
2022-01-07 19:43:07 +0900

    repository reset

--- src/Routes/SignUp/SignUpQuery.js
+++
@@ -1,7 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const SIGNUP = gql`
-mutation create($email: String!, $password: String!, $name: String!){
-    create(email: $email, password: $password, name: $name)
-}
-`;
--- src/apollo/tokenQuery.js
+++
@@ -1,7 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const TOKENINFO = gql`
-{
-    isLoggedIn @client
-}
-`;
