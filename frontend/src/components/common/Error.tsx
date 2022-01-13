e9a78124245ed02fa35652fc540fdd1d5437277b
leesh235 <leeshgh235@gmail.com>
2022-01-13 23:31:39 +0900

    add loding and error component

---
+++ frontend/src/components/common/Error.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+
+`;
+
+interface Props {
+
+}
+
+
+export const Error = ({}: Props) => {
+    return(
+        <Wrapper>error...</Wrapper>
+    );
+}
---
+++ frontend/src/components/common/Loding.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+
+`;
+
+interface Props {
+
+}
+
+
+export const Loading = ({}: Props) => {
+    return(
+        <Wrapper>loading...</Wrapper>
+    );
+}
