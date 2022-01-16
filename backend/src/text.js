45ce750a00947d08a14af59fb3a1da60240b163c
leesh235 <leeshgh235@gmail.com>
2022-01-16 22:33:46 +0900

    text file

---
+++ backend/src/text.js
@@ -0,0 +1,10 @@
+export const requiredLogin = "로그인이 필요합니다.";
+export const requiredInput = "필수 항목을 채워주세요.";
+
+export const successUnresister = "회원 탈퇴 완료";
+export const successSignup = "회원 가입 완료";
+
+export const existUser = "이미 가입한 이메일입니다.";
+export const defferrentPassword = "비밀번호가 틀렸습니다.";
+
+export const serverError = "server error";
