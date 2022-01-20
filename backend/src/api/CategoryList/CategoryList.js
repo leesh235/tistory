56fd5e09439485747f9a9fec7d667c80cc199478
leesh235 <leeshgh235@gmail.com>
2022-01-20 22:06:26 +0900

    add get category api

--- backend/src/api/CategoryList/CategoryList.js
+++ backend/src/api/CategoryList/CategoryList.js
@@ -1,23 +1,61 @@
 import { PrismaClient } from "@prisma/client";
 import { isAuthenticated } from "../../utile"
 import { SUCCESS, ERROR, SERVER_ERROR } from "../../constants/statusCode";
-import { REQUIRED_LOGIN, SUCCESS_DELETE_CATEGORY, ADDMIN_ERROR, EXIST_CATEGORY_POST } from "../../constants/message";
+import { SUCCESS_GET_CATEGORY, NOT_EXIST_CATEGORY } from "../../constants/message";
 
 const prisma = new PrismaClient();
 
 export default {
-    Mutation: {
+    Query: {
         getCategoryList: async(_, args, {request}) => {
             try{
                 const categoryList = await prisma.category.findMany({
                     where:{
                         parent: 0
+                    },
+                    orderBy: {
+                        sequence: 'asc'
                     }
                 });
 
-                const sub = await prisma.category.findMany({
-                    
-                })
+                const subCategoryList = await prisma.category.findMany({
+                    where:{
+                        NOT: {
+                            parent: 0
+                        }
+                    },
+                    orderBy: {
+                        sequence: 'asc'
+                    }
+                });
+
+                if(categoryList.length === 0 && subCategoryList.length === 0){
+                    return {
+                        __typename: "CategoryListFailure",
+                        status: ERROR,
+                        message: NOT_EXIST_CATEGORY,
+                    };
+                }
+
+                let arr = [];
+
+                for(let i = 0; i < categoryList.length; i++){
+                    let subArr = [];
+                    for(let j = 0; j < subCategoryList.length; j++){
+                        if(categoryList[i].id === subCategoryList[j].parent){
+                            subArr.push({id:subCategoryList[j].id, name: subCategoryList[j].name})
+                        }
+                    }
+                    arr.push({id:categoryList[i].id, name: categoryList[i].name, sub: subArr});         
+                }
+
+                return {
+                    __typename: "CategoryListSuccess",
+                    status: SUCCESS,
+                    message: SUCCESS_GET_CATEGORY,
+                    data: arr
+                };
+
 
             }catch(error){
                 throw new Error(SERVER_ERROR);
--- backend/src/constants/message.js
+++ backend/src/constants/message.js
@@ -7,12 +7,14 @@ export const SUCCESS_LOGIN = "로그인 성공";
 export const SUCCESS_GET_PROFILE = "유저 정보";
 export const SUCCESS_WRITE_CATEGORY = "카테고리 추가 완료";
 export const SUCCESS_DELETE_CATEGORY = "카테고리 삭제 완료";
+export const SUCCESS_GET_CATEGORY = "카테고리 가져오기 성공";
 
 export const EXIST_USER = "이미 가입한 이메일입니다.";
 export const NOT_EXIST_USER = "존재하지 않는 이메일입니다.";
 export const EXIST_DELETE = "이미 탈퇴한 이메일입니다.";
 export const EXIST_CATEGORY = "이미 존재하는 카테고리입니다.";
-export const EXIST_CATEGORY_POST = "해당 카테고리에 게시글이 존재합니다."
+export const EXIST_CATEGORY_POST = "해당 카테고리에 게시글이 존재합니다.";
+export const NOT_EXIST_CATEGORY = "카테고리가 존재하지 않습니다.";
 
 export const PASSWORD_ERROR = "비밀번호가 틀렸습니다.";
 
