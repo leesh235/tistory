e087c873d1d389fca8ebb05b62980af68eb54fec
leesh235 <leeshgh235@gmail.com>
2022-01-22 20:10:47 +0900

    add search post list

--- backend/src/api/Search/Search.js
+++ backend/src/api/Search/Search.js
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import { SUCCESS, ERROR, SERVER_ERROR } from "../../constants/statusCode";
+import { SUCCESS_GET_POSTLIST, NOT_EXIST_POSTLIST, REQUIRED_INPUT } from "../../constants/message";
 
 const prisma = new PrismaClient();
 
@@ -9,56 +11,84 @@ export default {
                 const { count, page, text } = args;
                 //모든 post 불러오기
 
-                if(text !== ""){
-                    const totalPost = await prisma.post.findMany({
-                        where: {
-                            title: {
-                                contains: text
-                            }
-                        }
-                    })
-                    const postLen = totalPost.length;
-                    //title에 text를 포함하고 있는 post 구하기
-                    const getTitle = await prisma.post.findMany({
-                        skip: (page - 1) * count,
-                        take: count,
-                        where: {
-                            title: {
-                                contains: text
-                            }
-                        }
-                    })
-                    // console.log(getTitle);
-
-                    if(getTitle !== null){
-                        return {
-                            search: getTitle,
-                            postCnt: postLen,
-                            status: "success"
-                        };
-                    }else{
-                        return {
-                            search: null,
-                            postCnt: 0,
-                            status: "no match"
-                        };
-                    }
-                    
-                }else{
+                if(text === ""){
                     return {
-                        search: null,
-                        postCnt: 0,
-                        status: "no search word"
+                        __typename: "SearchFailure",
+                        status: ERROR,
+                        message: REQUIRED_INPUT
+                    };
+                }
+                
+                //전체 게시글 수
+                const totalPost = await prisma.post.findMany({
+                    where: {
+                        title: {
+                            contains: text
+                        }
+                    }
+                })
+                const postLen = totalPost.length;
+
+                if(postLen === 0){
+                    return {
+                        __typename: "SearchFailure",
+                        status: ERROR,
+                        message: NOT_EXIST_POSTLIST
                     };
                 }
 
+                //title에 text를 포함하고 있는 post 구하기
+                const postList = await prisma.post.findMany({
+                    where: {
+                        title: {
+                            contains: text
+                        }
+                    },
+                    skip: (page - 1) * count,
+                    take: count,
+                    orderBy: {
+                        createAt:"desc"
+                    },
+                    select: {
+                        id: true,
+                        title: true,
+                        hits: true,
+                        createAt: true,
+                        author: {
+                            select: {
+                                email: true,
+                                nickName: true
+                            }
+                        },
+                        categories: {
+                            select: {
+                                name: true
+                            }
+                        }
+                    }
+                })
+
+                let result = [];
+                for(let i = 0; i < postList.length; i++){
+                    result.push({
+                        postId: postList[i].id,
+                        author: postList[i].author.email,
+                        title: `[${postList[i].categories.name}]`+postList[i].title,
+                        createAt: postList[i].createAt,
+                        hits: postList[i].hits,
+                        thumbnail: ""
+                    })
+                }
+
+                return {
+                    __typename: "SearchSuccess",
+                    status: SUCCESS,
+                    message:SUCCESS_GET_POSTLIST,
+                    data: result
+                };
+
             }catch (error){
-                console.log(error);
-                    return {
-                        search: null,
-                        postCnt: 0,
-                        status: "error"
-                    };
+                throw error;
             }
         }
     }
