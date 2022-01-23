accfa9704a21709851ac4f98bac00abe1ff8496e
leesh235 <leeshgh235@gmail.com>
2022-01-23 23:27:07 +0900

    create folder

--- backend/src/constants/message.js
+++ backend/src/constants/message.js
@@ -12,6 +12,7 @@ export const SUCCESS_WRITE_POST = "게시글 작성 성공";
 export const SUCCESS_GET_POST = "게시글 가져오기 성공";
 export const SUCCESS_GET_POSTLIST = "게시글 목록 가져오기 성공";
 export const SUCCESS_DELETE_POSTLIST = "게시글 삭제하기 성공";
+export const SUCCESS_WRITE_WRITE = "댓글 작성 성공";
 
 export const EXIST_USER = "이미 가입한 이메일입니다.";
 export const EXIST_DELETE = "이미 탈퇴한 이메일입니다.";
@@ -29,3 +30,5 @@ export const INCRRECT_AUTHOR = "작성자가 아닙니다.";
 export const PASSWORD_ERROR = "비밀번호가 틀렸습니다.";
 
 export const ADDMIN_ERROR = "관리자 권한이 없습니다.";
+
+export const WRONG_ACCESS = "잘못된 접근입니다.";
