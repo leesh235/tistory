208f21cab0435ef5016bd4c396dc08f540f39fb4
leesh235 <leeshgh235@gmail.com>
2022-01-26 17:50:47 +0900

    add get notice list api

---
+++ backend/src/api/NoticeList/NoticeList.js
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+import { SUCCESS, ERROR } from "../../constants/statusCode";
+import { SUCCESS_GET_NOTICELIST, NOT_EXIST_NOTICELIST } from "../../constants/message";
+
+const prisma = new PrismaClient();
+
+export default {
+    Query: {
+        getNoticeList: async(_,args) => {
+            try {
+                const { count, page } = args;
+
+                //전체 공지사항 수
+                const totalNotice = await prisma.notice.findMany({
+                    where: {
+                        deleteAt: null
+                    }
+                });
+
+                const NoticeLen = totalNotice.length;
+
+                if(NoticeLen === 0){
+                    return {
+                        __typename: "NoticeListFailure",
+                        status: ERROR,
+                        message:NOT_EXIST_NOTICELIST
+                    };
+                }
+
+                //해당 페이지의 공지사항 배열
+                const noticeList = await prisma.notice.findMany({
+                    where: {
+                        deleteAt: null
+                    },
+                    skip: (page - 1) * count,
+                    take: count,
+                    orderBy: {
+                        createAt: "desc"
+                    },
+                    select: {
+                        id: true,
+                        title: true,
+                        contents: true,
+                        createAt: true,
+                    }
+                })
+
+                return {
+                    __typename: "NoticeListSuccess",
+                    status: SUCCESS,
+                    message: SUCCESS_GET_NOTICELIST,
+                    data: noticeList
+                };
+
+            }catch (error){
+                throw error;
+            }
+        }
+    }
+}
--- backend/src/constants/message.js
+++ backend/src/constants/message.js
@@ -16,6 +16,7 @@ export const SUCCESS_WRITE_WRITE = "댓글 작성 성공";
 export const SUCCESS_GET_COMMENTLIST = "댓글 리스트 가져오기 성공";
 export const SUCCESS_MODIFY_COMMENT = "댓글 수정 성공";
 export const SUCCESS_WRITE_NOTICE = "공지사항 작성 성공";
+export const SUCCESS_GET_NOTICELIST = "공지사항 목록 가져오기 성공";
 
 export const EXIST_USER = "이미 가입한 이메일입니다.";
 export const EXIST_DELETE = "이미 탈퇴한 이메일입니다.";
@@ -26,6 +27,7 @@ export const NOT_EXIST_USER = "존재하지 않는 이메일입니다.";
 export const NOT_EXIST_CATEGORY = "카테고리가 존재하지 않습니다.";
 export const NOT_EXIST_POST = "해당 게시글이 존재하지 않습니다.";
 export const NOT_EXIST_POSTLIST = "게시글이 존재하지 않습니다.";
+export const NOT_EXIST_NOTICELIST = "공지사항이 존재하지 않습니다.";
 
 export const ALREADY_DELETE_POST = "이미 삭제된 게시글입니다.";
 export const ALREADY_DELETE_COMMENT = "이미 삭제된 댓글입니다.";
