46b70bbc9fe99a19d3c49c407c9517a65fbc143d
leesh235 <leeshgh235@gmail.com>
2022-01-28 23:00:20 +0900

    modify query(fileserver)

--- fileserver/src/modules/query.js
+++ fileserver/src/modules/query.js
@@ -1,19 +1,42 @@
 import { gql } from "@apollo/client";
 
-export const MODIFYUERIMG = gql`
-    mutation ModifyUserImg($userImg: Boolean){
-        ModifyUserImg(userImg: $userImg){
-            check
-            status
+export const MODIFYUSERIMAGE = gql`
+    mutation writeUserImage($imageUrl: String){
+        writeUserImage(imageUrl: $imageUrl){
+            ...on UserImageSuccess{
+                status
+                message
+                data: {
+                    id
+                    nickName
+                    imageUrl
+                }
+            }
+            ...on UserImageFailure{
+                status
+                message
+            }
         }
     }
 `;
 
-export const UPLOADTEXT = gql`
-    mutation uploadText($postId: Int){
-        uploadText(postId: $postId){
-            check
-            status
+export const EDITOR = gql`
+    mutation writeEditor($postId: Int! $contentsUrl: String){
+        writeEditor(postId: $postId contentsUrl: $contentsUrl){
+            ...on EditorSuccess{
+                status
+                message
+                data: {
+                    id
+                    title
+                    contentsUrl
+                    modifyAt
+                }
+            }
+            ...on EditorFailure{
+                status
+                message
+            }
         }
     }
 `;
--- fileserver/src/modules/uploadImg.js
+++ fileserver/src/modules/uploadImg.js
@@ -1,31 +1,23 @@
-import { MODIFYUERIMG, MODIFYPOSTIMG, UPLOADTEXT } from "./query.js";
-import  {makeClient}  from "../client.js";
+import { MODIFYUSERIMAGE, EDITOR } from "./query.js";
+import  { makeClient }  from "../client.js";
 
 export const profileToDB = async(req) => {
     const client = makeClient(req.headers.authorization);
-    if(req.body.streamfile !== ""){
-        return await client.mutate({
-            mutation: MODIFYUERIMG,
-            variables: {
-                userImg: true
-            }
-        });
-    }else{
-        return await client.mutate({
-            mutation: MODIFYUERIMG,
-            variables: {
-                userImg: false
-            }
-        });
-    }
+    return await client.mutate({
+        mutation: MODIFYUSERIMAGE,
+        variables: {
+            imageUrl: ""
+        }
+    });
 };
 
 export const postContentsToDB = async(req) => {
     const client = makeClient(req.headers.authorization);
     return await client.mutate({
-        mutation: UPLOADTEXT,
+        mutation: EDITOR,
         variables: {
-            postId: req.body.postId
+            postId: req.body.postId,
+            contentsUrl: ""
         }
     });
 };
