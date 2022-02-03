753a70e17e9076faf70465f53a3df23553037987
leesh235 <leeshgh235@gmail.com>
2022-02-03 23:15:50 +0900

    add fuction

--- frontend/src/containers/LogInContainer.tsx
+++ frontend/src/containers/LogInContainer.tsx
@@ -7,6 +7,30 @@ export const LogInContainer = () => {
     const [loginMutation] = useMutation(LOGIN);
     const [tokenMutation] = useMutation(TOKENLOGIN);
 
+    const handleSubmit = async() => {
+        const result = await loginMutation({
+            variables: {
+                email: "", 
+                password: ""
+            }
+        })
+
+        if(result !== "LogInSuccess"){
+
+        }else{
+            const { data: {
+                status,
+                message,
+                data,
+            }} = result;
+            await tokenMutation({
+                variables: {
+                    token: data.token
+                }
+            })
+        }
+    }
+
     return (
         <LogInPresenter />
     );
