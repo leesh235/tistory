7471153bf8cf7f51db01a2bac1e9be5e2fbb421a
leesh235 <leeshgh235@gmail.com>
2022-02-06 21:42:35 +0900

    create profile page

---
+++ frontend/src/pages/Profile.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.main`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+`;
+
+const Profile = () => {
+    return(
+        <Wrapper>
+            profile
+        </Wrapper>
+    );
+}
+
+export default Profile;
