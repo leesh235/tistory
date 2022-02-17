416efdf01a03f23cfa36d07b14b950124ce50875
leesh235 <leeshgh235@gmail.com>
2022-02-18 00:06:46 +0900

    fix bug

--- frontend/src/utils/responsive.tsx
+++ frontend/src/utils/responsive.tsx
@@ -1,12 +1,21 @@
-import { FC } from 'hoist-non-react-statics/node_modules/@types/react';
+import React, { ReactChildren, ReactChild } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
-export const PC = ({children } : { children: FC }) => {
-	const isPC = useMediaQuery({query:"(min-width: 64em)"})
-	return isPC ? children : null
+interface Props{
+	children?: ReactChildren | ReactChild | React.ReactNode
 }
 
-export const Mobile = ({children} : { children: FC }) => {
-	const isMobile = useMediaQuery({query:"(max-width: 48em)"})
-	return isMobile ? children : null
+export const PC = ({children} : Props) => {
+	const isPC = useMediaQuery({query:"(min-width: 64em)"})
+	return isPC ? <React.Fragment>{children}</React.Fragment> : null
+}
+
+export const Tablet = ({children} : Props) => {
+	const isTablet = useMediaQuery({query:"(max-width: 64em) and (min-width: 22.5em)"})
+	return isTablet ? <React.Fragment>{children}</React.Fragment> : null
+}
+
+export const Mobile = ({children} : Props) => {
+	const isMobile = useMediaQuery({query:"(max-width: 22.5em)"})
+	return isMobile ? <React.Fragment>{children}</React.Fragment> : null
 }
