004bc6e7868c4ccd6e94af7cfc3367782eb618bb
leesh235 <leeshgh235@gmail.com>
2022-02-21 23:22:28 +0900

    add more type

--- frontend/src/utils/responsive.tsx
+++ frontend/src/utils/responsive.tsx
@@ -19,3 +19,18 @@ export const Mobile = ({children} : Props) => {
 	const isMobile = useMediaQuery({query:"(max-width: 22.44em)"})
 	return isMobile ? <React.Fragment>{children}</React.Fragment> : null
 }
+
+export const PCTablet = ({children} : Props) => {
+	const isPCTablet = useMediaQuery({query:"(min-width: 22.5em)"})
+	return isPCTablet ? <React.Fragment>{children}</React.Fragment> : null
+}
+
+export const TabletMobile = ({children} : Props) => {
+	const isPCTabletMobile = useMediaQuery({query:"(max-width: 63.94em)"})
+	return isPCTabletMobile ? <React.Fragment>{children}</React.Fragment> : null
+}
+
+export const PCMobile = ({children} : Props) => {
+	const isPCMobile = useMediaQuery({query:"(min-width: 64em) and (max-width: 22.44em)"})
+	return isPCMobile ? <React.Fragment>{children}</React.Fragment> : null
+}
