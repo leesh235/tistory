4d29fa1747b739f8679c6a5d8582ace13f36bbf5
leesh235 <leeshgh235@gmail.com>
2022-02-21 20:10:43 +0900

    add hooks

---
+++ frontend/src/Hooks/useScroll.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import debounce from 'lodash/debounce';
+
+export function useScroll() {
+  const [scrollY, setScrollY] = useState<number>(0);
+
+  const listener = () => {
+    if(window.pageYOffset < 101){
+        setScrollY(window.pageYOffset);
+    }
+  };
+
+  const delay = 15;
+
+  useEffect(() => {
+    window.addEventListener('scroll', debounce(listener, delay));
+    return () => window.removeEventListener('scroll', listener);
+  });
+
+  return {
+    scrollY
+  };
+}
