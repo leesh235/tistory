f7688858d7873461be63dcf00eb64b8032102099
leesh235 <leeshgh235@gmail.com>
2022-02-22 23:06:57 +0900

    add select component

---
+++ frontend/src/assets/svg/Arrow.js
@@ -0,0 +1,17 @@
+export const ArrowUp = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M12 10.828l-4.95 4.95-1.414-1.414L12 8l6.364 6.364-1.414 1.414z"/>
+        </svg>
+    );
+}
+
+export const ArrowDown = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z"/>
+        </svg>
+    );
+}
---
+++ frontend/src/components/common/Select.tsx
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { ArrowDown, ArrowUp } from "../../assets/svg/Arrow";
+import { Text } from "./Text";
+
+const Wrapper = styled.div<StyleProps>`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: ${props => props.width};
+    height: ${props => props.height};
+    border-radius: 6px;
+    border: 1px solid gray;
+    background-color: white;
+    position: relative;
+`;
+
+const SelectWrapper = styled.div<StyleProps>`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: calc(100% - 2px);
+    height: calc(100% - 2px);
+    cursor: pointer;
+
+`;
+
+const OptionWrapper = styled.ul<StyleProps>`
+    display: ${props => props.display};
+    flex-direction: column;
+    width: 100%;
+    height: auto;
+    position: absolute;
+    top: 32px;
+    z-index: 9999;
+    border-radius: 6px;
+    border: 1px solid gray;
+    background-color: white;
+    li{
+        display: flex;
+        align-items: center;
+        width: 100%;
+        height: 30px;
+        cursor: pointer;
+        margin: 5px 0;
+    }
+`;
+
+const ArrowWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+interface StyleProps{
+    width?: string,
+    height?: string,
+    display?: string
+}
+
+interface Props extends StyleProps{
+    option: Array<string>,
+    defaultOption: string,
+}
+
+export const Select = ({width, height, defaultOption, option}: Props) => {
+
+    const [open, setOpen] = useState<boolean>(false);
+    const [select, setselect] = useState<string>(defaultOption);
+
+    const handleOpen = () => {
+        setOpen(!open);
+    }
+
+    const handleSelect = (val: string) => {
+        setselect(val);
+        setOpen(false);
+    }
+
+    return(
+        <Wrapper width={width} height={height}>
+            <SelectWrapper onClick={handleOpen}>
+                <Text text={select} fc={"gray"} fs={"1.4rem"}/>
+                <ArrowWrapper>
+                    {open ? <ArrowUp /> : <ArrowDown />}
+                </ArrowWrapper>
+            </SelectWrapper>
+            <OptionWrapper  display={open ? "flex" : "none"}>
+                {option.map((val, idx) => {
+                    return(
+                        <li key={idx} onClick={() => {handleSelect(val)}}>
+                            <Text text={val} fc={"gray"} fs={"1.4rem"}/>
+                        </li>
+                    );
+                })}
+            </OptionWrapper>
+        </Wrapper>
+    );
+}
+
+Select.defaultProps = {
+    width: "282px",
+    height: "30px",
+    defaultOption: "카테고리를 선택해주세요",
+    option: ["공지사항"],
+}
