fa1b1d16b07172bc7706944347f6327fdee3b07e
leesh235 <leeshgh235@gmail.com>
2022-02-23 15:58:07 +0900

    fix bug

--- frontend/src/redux/actions/categoryList.ts
+++ frontend/src/redux/actions/categoryList.ts
@@ -1,6 +1,6 @@
 export const CATEGORYLIST_ACTION_TYPE = "categoryList" as const;
 
-export const categoryList = (data: Array<{id: number, name: string}>) => {
+export const setCategoryList = (data: Array<{id: number, name: string}>) => {
     return {
         type: CATEGORYLIST_ACTION_TYPE,
         data: data
--- frontend/src/redux/reducers/index.ts
+++ frontend/src/redux/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import category from "./category";
 import sideMenu from "./sideMenu";
+import categoryList from "./categoryList";
 
 const allReducer = combineReducers({
     category,
-    sideMenu
+    sideMenu,
+    categoryList
 });
 
 export default allReducer;
