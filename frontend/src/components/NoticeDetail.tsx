e697607f70d0d04eb1b6ccb1145dd9717fac3731
leesh235 <leeshgh235@gmail.com>
2022-02-26 23:47:39 +0900

    add notice detail

--- frontend/src/components/NoticeDetail.tsx
+++ frontend/src/components/NoticeDetail.tsx
@@ -1,14 +1,50 @@
 import styled from 'styled-components';
 import { PC, Tablet, Mobile } from '../utils/responsive';
+import moment from "moment";
+import { Text } from "./common/Text";
+import { LineStyle } from "./LineStyle";
 
 const Wrapper = styled.section`
-
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: auto;
+    align-items: center;
 `;
 
-export const NoticeDetail = () => {
+const TopWrapper = styled.article`
+    width: 95%;
+    height: auto;
+    margin: 20px 0 0 0;
+`;
+
+const ContentWrapper = styled.article`
+    display: flex;
+    flex-direction: column;
+    width: 95%;
+    height: auto;
+    >:nth-child(n){
+        margin-bottom: 50px;
+    }
+`;
+
+interface Props {
+    title: string,
+    contents: string,
+    createAt: string,
+}
+
+export const NoticeDetail = ({ title, contents, createAt }: Props) => {
     return(
         <Wrapper>
-            NoticeDetail
+            <TopWrapper>
+                <Text text={`Leesh | ${moment(createAt).format("YYYY-MM-DD")}`} fs={"1.3rem"} fc={"gray"}/>
+                <h2>{title}</h2>
+            </TopWrapper>
+
+            <LineStyle w={"98%"} margin={"20px 0"}/>
+
+            <Text text={`${contents}`} fc={"black"}/>
         </Wrapper>
     );
 }
--- frontend/src/containers/NoticeContainer.tsx
+++ frontend/src/containers/NoticeContainer.tsx
@@ -1,12 +1,29 @@
 import { NOTICE } from '../querys/NoticeQuery';
 import { useQuery } from '@apollo/client';
 import { NoticeDetail } from '../components/NoticeDetail';
+import { useParams } from 'react-router-dom';
+import { Loading } from '../components/common/Loding';
+import { Error } from '../components/common/Error';
 
 export const NoticeContainer = () => {
 
-    const { loading, data, error } = useQuery(NOTICE);
+    const { id } = useParams<{id: string}>();
 
-    return(
-        <NoticeDetail />
-    );
+    const { loading, error, data } = useQuery(NOTICE,{
+        variables: {
+            noticeId: Number(id)
+        }
+    });
+
+    if(loading) return <Loading /> 
+    else if(error) return <Error />
+    else{
+        return(
+            <NoticeDetail 
+                title={data?.getNoticeDetail?.data?.title}
+                contents={data?.getNoticeDetail?.data?.contents}
+                createAt={data?.getNoticeDetail?.data?.createAt}
+            />
+        );
+    }
 }
