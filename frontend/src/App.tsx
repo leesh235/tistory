2b1245960124e0d82e740a2da008f94c9e4942d8
leesh235 <leeshgh235@gmail.com>
2022-03-03 22:02:11 +0900

    add login control

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,23 @@ import { GlobalStyle } from "./styles/GlobalStyle";
 import { theme } from "./styles/theme";
 import Routes from './components/Routes';
 
+import { useQuery } from '@apollo/client';
+import { TOKENINFO } from "./apollo/tokenQuery";
+import { PROFILE } from "./querys/ProfileQuery";
+import { useDispatch } from "react-redux";
+import { setUser } from "./redux/actions/user";
+
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  const getLogInInfo = useQuery(TOKENINFO);
+
+  const getUserInfo = useQuery(PROFILE);
+
+  if(getLogInInfo?.data?.isLoggedIn){
+    dispatch(setUser(getUserInfo?.data?.getProfile?.data.role));
+  }
   
   return (
     <ThemeProvider theme={theme}>
