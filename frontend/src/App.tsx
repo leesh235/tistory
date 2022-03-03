cacb5d196b593fecfa781996938e87c736f333a7
leesh235 <leeshgh235@gmail.com>
2022-03-03 20:26:31 +0900

    refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,14 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { theme } from "./styles/theme";
 import Routes from './components/Routes';
-import { Provider } from 'react-redux';
-import createStore from './redux/store';
-import reducers from "./redux/reducers/index";
-
-const store = createStore(reducers);
 
 const App = () => {
   
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Routes />
-      </ ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Routes />
+    </ ThemeProvider>
   )
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ApolloProvider }from '@apollo/client';
+import { Provider } from 'react-redux';
 import client from './apollo/apollo';
+import createStore from './redux/store';
+import reducers from "./redux/reducers/index";
 import "./env";
+
+const store = createStore(reducers);
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
