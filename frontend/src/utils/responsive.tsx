62fba317ce83540a3f85bfab8d14645ce139ef8a
leesh235 <leeshgh235@gmail.com>
2022-03-08 16:20:50 +0900

    add screen types

--- frontend/src/utils/responsive.tsx
+++ frontend/src/utils/responsive.tsx
@@ -5,32 +5,39 @@ interface Props{
 	children?: ReactChildren | ReactChild | React.ReactNode
 }
 
+export const is_pc = "(min-width: 64em)";
+export const is_tablet = "(max-width: 63.94em) and (min-width: 22.5em)";
+export const is_mobile = "(max-width: 22.44em)";
+export const is_pc_tablet = "(min-width: 22.5em)";
+export const is_tablet_mobile = "(max-width: 63.94em)";
+export const is_pc_mobile = "(min-width: 64em) and (max-width: 22.44em)";
+
 export const PC = ({children} : Props) => {
-	const isPC = useMediaQuery({query:"(min-width: 64em)"})
+	const isPC = useMediaQuery({query: is_pc})
 	return isPC ? <React.Fragment>{children}</React.Fragment> : null
 }
 
 export const Tablet = ({children} : Props) => {
-	const isTablet = useMediaQuery({query:"(max-width: 63.94em) and (min-width: 22.5em)"})
+	const isTablet = useMediaQuery({query: is_tablet})
 	return isTablet ? <React.Fragment>{children}</React.Fragment> : null
 }
 
 export const Mobile = ({children} : Props) => {
-	const isMobile = useMediaQuery({query:"(max-width: 22.44em)"})
+	const isMobile = useMediaQuery({query: is_mobile})
 	return isMobile ? <React.Fragment>{children}</React.Fragment> : null
 }
 
 export const PCTablet = ({children} : Props) => {
-	const isPCTablet = useMediaQuery({query:"(min-width: 22.5em)"})
+	const isPCTablet = useMediaQuery({query: is_pc_tablet})
 	return isPCTablet ? <React.Fragment>{children}</React.Fragment> : null
 }
 
 export const TabletMobile = ({children} : Props) => {
-	const isPCTabletMobile = useMediaQuery({query:"(max-width: 63.94em)"})
-	return isPCTabletMobile ? <React.Fragment>{children}</React.Fragment> : null
+	const isTabletMobile = useMediaQuery({query: is_tablet_mobile})
+	return isTabletMobile ? <React.Fragment>{children}</React.Fragment> : null
 }
 
 export const PCMobile = ({children} : Props) => {
-	const isPCMobile = useMediaQuery({query:"(min-width: 64em) and (max-width: 22.44em)"})
+	const isPCMobile = useMediaQuery({query: is_pc_mobile})
 	return isPCMobile ? <React.Fragment>{children}</React.Fragment> : null
 }
